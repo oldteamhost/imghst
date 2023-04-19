f0994624a1da60267e8905618111d692c9bc8898
lomaster <119946584+Lomasterrrr@users.noreply.github.com>
2023-04-19 18:05:38 +0300

    Delete clearcurl.h

--- clearcurl.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "curl/curl.h"
-
-size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream);
-size_t clear_data(void* buffer, size_t size, size_t nmemb, void* userp);
